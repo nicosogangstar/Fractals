776227d54518213d8e93d5ddc89f79b9d9c01c3a
Nicolas Gonzalez <nicolasnicogonzalez@gmail.com>
2016-12-01 21:28:27 -0500

    added toolbar/navigation drawer

--- app/src/main/java/com/nico/gonzo/fractals/ViewerActivity.java
+++ app/src/main/java/com/nico/gonzo/fractals/ViewerActivity.java
@@ -1,20 +1,27 @@
 package com.nico.gonzo.fractals;
 
 import android.app.Activity;
-import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.LinearLayout;
 
-public class ViewerActivity extends Activity {
-
-    private GLSurfaceView mGLView;
+public class ViewerActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Create a GLSurfaceView instance and set it
-        // as the ContentView for this Activity.
-        mGLView = new MyGLSurfaceView(this);
-        setContentView(mGLView);
+        setContentView(R.layout.activity_viewer);
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.fractalView);
+        linearLayout.addView(new MyGLSurfaceView(this));
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar actionBar = getSupportActionBar();
+
     }
 }
