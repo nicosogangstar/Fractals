8bb05fa8da2e3277ba32b8ac0bb05ddeec8d9f47
Nicolas Gonzalez <nicolasnicogonzalez@gmail.com>
2016-12-30 18:50:07 -0500

    added D3 Mandelbrot

--- app/src/main/assets/mandelbrot.fs.glsl
+++ app/src/main/assets/mandelbrot.fs.glsl
@@ -7,11 +7,11 @@ uniform vec4 bounds;
 
 // z = x + yi
 // c = x0 + y0
-// z^2 + c = x^2 + 2yxi - y^2 + x0 + i*y0
 
+// z^2 + c = x^2 + 2yxi - y^2 + x0 + i*y0
 float real(vec2 z, float cx) {
     // x^2 - y^2 + x0
-    return pow(z.x, 2.0) - pow(z.y, 2.0) + cx;
+    return (z.x * z.x) - (z.y * z.y) + cx;
 }
 
 float imaginary(vec2 z, float cy) {
@@ -19,12 +19,21 @@ float imaginary(vec2 z, float cy) {
     return 2.0 * z.x * z.y + cy;
 }
 
+// z^3 + c = x^3 + 3iyx^2 - 3xy^2 - iy^2 + x0 + iy0
+float realD3(vec2 z, float cx) {
+    return (z.x * z.x * z.x) - 3.0 * z.x * (z.y * z.y) + cx;
+}
+
+float imaginaryD3(vec2 z, float cy) {
+    return 3.0 * z.y * (z.x * z.x) - (z.y * z.y * z.y) + cy;
+}
+
 float mandelbrot(vec2 c) {
     vec2 z = c;
     float its = 0.0;
     for(int i = 0; i < int(maxIterations); i++) {
-        float t = imaginary(z, c.y);
-        z.x = real(z, c.x);
+        float t = imaginaryD3(z, c.y);
+        z.x = realD3(z, c.x);
         z.y = t;
 
         if(z.x * z.x + z.y * z.y > 4.0) {
